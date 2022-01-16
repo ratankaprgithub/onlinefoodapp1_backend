2cf5a274d38fe6462aec80c9fd50e23cc6752a56
Ratan <ratankapr22@gmail.com>
2022-01-17 00:42:02 +0530

    slf4J added

--- onlinefooddeliveryapp/src/main/java/com/cg/app/OnlinefooddeliveryappApplication.java
+++ onlinefooddeliveryapp/src/main/java/com/cg/app/OnlinefooddeliveryappApplication.java
@@ -3,10 +3,16 @@ package com.cg.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class OnlinefooddeliveryappApplication {
 
+	
 	public static void main(String[] args) {
+	
+		log.info("application starts...");
 		SpringApplication.run(OnlinefooddeliveryappApplication.class, args);
 	}
 
